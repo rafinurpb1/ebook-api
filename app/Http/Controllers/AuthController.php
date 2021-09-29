3ec1aa2bc257aa719b550e3acb70a56b6d30cdab
ZalEmperon <flowgreen173@gmail.com>
2021-09-29 21:27:05 +0700

    Migration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,13 +6,11 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function me() {
-        return [
-            'NIS' => '3103119002',
-            'Name' => 'Ade Firmansyah',
-            'Gender' => 'Pria',
-            'Phone' => '0895384049671',
-            'Class' => 'XII RPL 1'
-        ];
-    }
+  public function me() {
+    return ['NIS' => 3103119150,
+        'name' => 'Rafi Nur Fauzan',
+        'gender' => 'Male',
+        'phone' => '08574686359874',
+        'class' => 'XII RPL 5'];
+  }
 }
